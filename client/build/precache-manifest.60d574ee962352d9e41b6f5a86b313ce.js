22c737c14fbce5545c61ab5cc5a8da2ffa89903e
yourName <ivangr21@gmail.com>
2020-03-20 16:22:58 -0400

    JUST DEV FOR NOW

---
+++ client/build/precache-manifest.60d574ee962352d9e41b6f5a86b313ce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c26bd2678778d7640efe987268ab06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ca726ce43064e63ef5c",
+    "url": "/static/css/2.4b624d99.chunk.css"
+  },
+  {
+    "revision": "1d04ecbed6fcbbc06f24",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "1ca726ce43064e63ef5c",
+    "url": "/static/js/2.3e63cf31.chunk.js"
+  },
+  {
+    "revision": "1d04ecbed6fcbbc06f24",
+    "url": "/static/js/main.ca799e60.chunk.js"
+  },
+  {
+    "revision": "e5dec4a10a3bf17bf564",
+    "url": "/static/js/runtime-main.35b63598.js"
+  },
+  {
+    "revision": "79cabd8e744cd493c1297505d61b755b",
+    "url": "/static/media/hero2.79cabd8e.jpg"
+  },
+  {
+    "revision": "587b0c4097733fd26196b6beadaa1285",
+    "url": "/static/media/hero3.587b0c40.png"
+  },
+  {
+    "revision": "dbffe6a1fa6e1bbc30ffe8746ab4855b",
+    "url": "/static/media/logo.dbffe6a1.jpg"
+  },
+  {
+    "revision": "33a05a05ca6d8fec016270b236740a1d",
+    "url": "/static/media/logo2.33a05a05.jpg"
+  }
+]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,24 @@
- 
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var public_dir = "src/frontend";
+var ModernizrWebpackPlugin = require('modernizr-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, public_dir , 'index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    plugins
+  ],
+  module: {
+    loaders: [loaders]
+  }
+};
